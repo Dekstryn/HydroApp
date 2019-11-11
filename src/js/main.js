48ace9bc7e99da012f770c1fbc954f6c57be4bf3
Krzysztof Nyrek <krzysztof.nyrek@gmail.com>
2019-11-11 11:01:46 +0100

    Add localstorage used to initial glass count

--- src/js/main.js
+++ src/js/main.js
@@ -15,26 +15,28 @@ if ('serviceWorker' in navigator) {
 }
 
 // Variables definition
-let glass = 0;
 let d = new Date();
 let day = d.getDay(); //set day as number starting from monday = 1
+let dayStr = JSON.stringify(day);
+let glassStr = localStorage.getItem(dayStr);
+let glass = JSON.parse(glassStr);
+
+if(glass==null){
+  glass = 0;
+}
+console.log(glassStr, glass) //just for test 
+
 const counter = document.querySelector('.application__count--js');
 const buttonAdd = document.querySelector('.interaction__add--js');
 const buttonRemove = document.querySelector('.interaction__remove--js');
 
-
-//Save variables in local storage
-let glassStr = JSON.stringify(glass);
-let dayStr = JSON.stringify(day);
-localStorage.setItem(dayStr, glassStr);
-//localStorage.setItem('daySave', dayStr);
+//set starter glass value display
+counter.textContent = `${glass}`;
 
 buttonAdd.addEventListener('click', (e) =>{
-  glassStr == localStorage.getItem(dayStr);
-  glass == JSON.parse(glassStr);
   glass++;
   glassStr = JSON.stringify(glass);
+  let dayStr = JSON.stringify(day);
   localStorage.setItem(dayStr, glassStr);
   counter.textContent = `${glass}`;
-  console.log(glass, day) //just for test 
 })
